c05fd2ea2c6f7fa1c9b9b667869ea22519f2db55
CBStonesifer <cbstonesifer@gmail.com>
2024-05-15 21:55:08 -0400

    First commit

---
+++ rankroster/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  rankroster
+//
+//  Created by Carter Stonesifer on 5/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Getting Started")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ rankroster/rankrosterApp.swift
@@ -0,0 +1,17 @@
+//
+//  rankrosterApp.swift
+//  rankroster
+//
+//  Created by Carter Stonesifer on 5/15/24.
+//
+
+import SwiftUI
+
+@main
+struct rankrosterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
